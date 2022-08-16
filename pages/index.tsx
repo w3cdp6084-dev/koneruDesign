68a60024e4fdf84bbb73755fad2a4c6097e8af24
w3cdp6084-dev <yusukew3@gmail.com>
2022-08-17 06:58:05 +0900

    set list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,35 @@
 import { client } from '../libs/client';
+import type { Blog } from '../types/blog';
 
-export default function Home() {
+type Props = {
+  blog: Array<Blog>;
+};
+export default function Home({ blog }: Props) {
   return (
     <>
       <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
-        記事一覧
+       記事一覧
       </h1>
       <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-        <div className="rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4">テストタイトル</div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              # テストタグ
-            </span>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
-export const getServerSideProps = async () => {
-  const data = await client.get({ endpoint: 'blog' });
+       {blog.map(blog => (
+         <div className="rounded overflow-hidden shadow-lg" key={blog.id}>
 
+           <div className="px-6 py-4">{blog.title}</div>
+           <div className="px-6 pt-4 pb-2">
+           {blog.tag && (
+             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+               #{blog.tag}
+             </span>
+           )}
+           </div>
+         </div>
+       ))}
+     </div>
+    </>
+    );
+  }
+export const getServerSideProps = async () => {
+  const data = await client.get({ endpoint: 'blog',queries: { limit: 4, offset: 0}});
   return {
     props: {
       blog: data.contents,
