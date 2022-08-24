826b478763ec5f2943b4a612b2415e4a863e5e92
w3cdp6084-dev <yusukew3@gmail.com>
2022-08-24 15:47:12 +0900

    masonry uninstall

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.scss';
 import '../styles/style.scss';
+import * as React from "react";
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 
