ef088b877113e18a4e1a4ade268d1eec71a77a58
w3cdp6084-dev <yusukew3@gmail.com>
2022-08-31 10:40:34 +0900

    404 実装

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,10 @@
+import styles from '../styles/components/404.module.scss';
 export default function Custom404() {
     return (
       <main className="main max-w-6xl px-10 py-6 mx-auto">
         <div className="">
-          <h2 className="text-center u-title-404">404</h2>
-          <p className="text-center text-4xl">🐱ページがありません🐱</p>
+          <h2 className={styles.uTitle404}>404</h2>
+          <p className={styles.caption}>🐱ページがありません🐱</p>
         </div>
       </main>
     );
