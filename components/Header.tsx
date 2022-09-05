d3662a8f1d80d16e2b9cf2998247ff04a8f07c49
w3cdp6084-dev <yusukew3@gmail.com>
2022-09-05 10:00:00 +0900

    Dark Mode delete

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,33 +2,33 @@ import Link from 'next/link'
 import styles from '../styles/components/Header.module.scss';
 export default function Header() {
   return (
-    <header className={styles.wrap}>
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link href={'/'} passHref>
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="text-3xl ml-3 text-white font-bold">LOGO</span>
-          </a>
-        </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <Link href="/">
-            <a className="tracking-wide mr-5 text-white font-bold">HOME</a>
+      <header className={styles.wrap}>
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <Link href={'/'} passHref>
+            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+              <span className="text-3xl ml-3 text-white font-bold">LOGO</span>
+            </a>
           </Link>
-          <Link href="/about">
-            <a className="tracking-wide mr-5 text-white font-bold">ABOUT</a>
-          </Link>
-          <Link href="/">
-            <a className="tracking-wide mr-5 text-white font-bold">CONTACT</a>
-          </Link>
-          <form>   
-            <div className="relative">
-              <input type="search" id="default-search" className="block p-4 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 inputOutline" placeholder="Test" required/>
-                <div className="flex absolute inset-y-0 right-0 items-center pl-3 pointer-events-none searchBtn">
-                  <img src="../images/face01.png" alt="icon" width={32} />
-                </div>
-            </div>
-          </form>
-        </nav>
-      </div>
-    </header>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <Link href="/">
+              <a className="tracking-wide mr-5 text-white font-bold">HOME</a>
+            </Link>
+            <Link href="/about">
+              <a className="tracking-wide mr-5 text-white font-bold">ABOUT</a>
+            </Link>
+            <Link href="/">
+              <a className="tracking-wide mr-5 text-white font-bold">CONTACT</a>
+            </Link>
+            <form>   
+              <div className="relative">
+                <input type="search" id="default-search" className="block p-4 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 inputOutline" placeholder="Test" required/>
+                  <div className="flex absolute inset-y-0 right-0 items-center pl-3 pointer-events-none searchBtn">
+                    <img src="../images/face01.png" alt="icon" width={32} />
+                  </div>
+              </div>
+            </form>
+          </nav>
+        </div>
+      </header>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,11 @@ import '../styles/style.scss';
 import * as React from "react";
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
+    <div className='wrap'>
+        <Header />
+        <Component {...pageProps} />
     </div>
   );
 }
