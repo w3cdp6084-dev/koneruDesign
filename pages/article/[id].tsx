a017133d7288674c7e873c204aad4a229c7f3e41
w3cdp6084-dev <yusukew3@gmail.com>
2022-09-05 09:12:13 +0900

    add moment

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next';
 import type { Blog } from '../../types/blog';
 import { client } from '../../libs/client';
+import Moment from 'react-moment'
 import ReturnTopButton from '../../components/ReturnTopButton';
 type Props = {
     blog: Blog;
@@ -8,23 +9,28 @@ type Props = {
 
 export default function Article({ blog }: Props) {
   return (
-    <div className="">
+    <div className="w-full mx-auto">
       <div className="px-10 py-6 mx-auto">
-        <div className="max-w-6xl px-10 py-6 mx-auto bg-gray-50">
-          <div className="mt-2">
-            <div className="sm:text-3xl md:text-3xl lg:text-3xl xl:text-4xl font-bold text-blue-500">
+        <div className="max-w-6xl px-10 py-6 mx-auto">
+          <div className='my-6 w-10/12 mx-auto flex justify-start items-center'>
+            <Moment format="YYYY/MM/DD" className='text-xs font-bold date mr-10 text-white'>
+              {blog.publishedAt}
+            </Moment>
+            <p className='text-sm text-white py-1 px-2 category'>#{blog.category.name}</p>
+          </div>
+          <div className="mt-2 w-10/12 mx-auto">
+            <div className="tracking-wider sm:text-3xl md:text-3xl lg:text-3xl xl:text-4xl font-bold text-white">
               {blog.title}
             </div>
           </div>
-          {blog.tag && (
-            <div className="flex items-center justify-start mt-4 mb-4">
-              <div className="px-2 py-1 font-bold bg-red-400 text-white rounded-lg">
-                #{blog.tag}
-              </div>
-            </div>
-          )}
-          <div className="mt-2">
-            <div className="text-2xl text-gray-700 mt-4 rounded" dangerouslySetInnerHTML={{__html: blog.body}}></div>
+          <div className="mt-2 w-10/12 mx-auto">
+            <div className="text-sm leading-relaxed tracking-wider text-white mt-4 rounded" dangerouslySetInnerHTML={{__html: blog.body}}></div>
+          </div>
+          <div className='mx-auto w-3/5'>
+            <img
+              className="my-10 rounded-2xl object-cover shadow-sm"
+              src={blog.thumbnail.url}
+            />
           </div>
         </div>
         <ReturnTopButton />
