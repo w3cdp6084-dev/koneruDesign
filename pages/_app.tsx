cd78af1c2ccbb9b6edae68e66f70051ca956237c
w3cdp6084-dev <yusukew3@gmail.com>
2022-09-17 08:33:02 +0900

    progress add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import '../styles/style.scss';
 import * as React from "react";
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
+import NextNprogress from 'nextjs-progressbar'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='wrap'>
+        <NextNprogress /> 
         <Header />
         <Component {...pageProps} />
     </div>
